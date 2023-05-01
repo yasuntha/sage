0e794ea74d97d76382f6f97f5d08813e9473420d
yasuntha <131638732+yasuntha@users.noreply.github.com>
2023-05-02 02:08:18 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,39 @@
 const venom = require('venom-bot');
+const qrcode = require('qrcode-terminal');
 
-// start the client
 venom.create().then((client) => {
-
-  // define the phone number to send the message to
-  const phoneNumber = '94741090547@c.us';
-
-  // define the message text for the "Hi" button
-  const messageTextHi = 'Hello! How can I assist you today?';
-
-  // define the message text for the "Bye" button
-  const messageTextBye = 'Goodbye! Have a nice day.';
-
-  // define the button objects
-  const buttonHi = {
-    buttonText: 'Hi',
-    buttonId: 'btn_hi',
-    hideText: false
-  };
-
-  const buttonBye = {
-    buttonText: 'Bye',
-    buttonId: 'btn_bye',
-    hideText: false
-  };
-
-  // define the message object with the buttons
-  const message = {
-    phone: phoneNumber,
-    content: messageTextHi,
-    buttons: [buttonHi, buttonBye]
-  };
-
-  // send the message with buttons
-  client.sendButtons(message).then((result) => {
-
-    // check if the message was sent successfully
-    if (result.success) {
-      console.log('Message sent successfully!');
-    } else {
-      console.error('Failed to send message.');
-    }
-
-    // log out of WhatsApp
-    client.close().then(() => console.log('Client closed.'));
-
-  }).catch((error) => {
-    console.error(error);
-  });
-
-}).catch((error) => {
-  console.error(error);
+    start(client);
 });
+
+function start(client) {
+    client.onMessage(async (message) => {
+        if (message.body === '.hi') {
+            const text = `Hi, this is echo.`
+            const content = { 'message': { 'text': text } }
+            const options = {
+                "reply_markup": {
+                    "inline_keyboard": [
+                        [
+                            {
+                                "text": "Owner",
+                                "url": "https://wa.me/94741090547"
+                            },
+                            {
+                                "text": "Github",
+                                "url": "https://github.com/yasuntha/sage"
+                            }
+                        ]
+                    ]
+                }
+            }
+            await client.sendMessage(message.from, content, options);
+        }
+    });
+
+    client.onStateChange((state) => {
+        if (state.qrcode) {
+            console.log('QR Code:');
+            qrcode.generate(state.qrcode, { small: true });
+        }
+    });
+}
